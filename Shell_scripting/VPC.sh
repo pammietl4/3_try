912a8ac213ba16c694220ea8ecc03aad82d0a123
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-29 12:52:22 +0000

    script modified

--- Shell_scripting/VPC.sh
+++ Shell_scripting/VPC.sh
@@ -2,10 +2,10 @@
 
 #This script is used to get the list of VPCs from AWS
 
-Cat test.sh | jq ".[].ip" -r # finding the ip's from a file
+#Cat test.sh | jq ".[].ip" -r # finding the ip's from a file
 
-aws ec2 describe-vpcs | jq ".Vpcs[].VpcId"    # IP's with invited comas
-aws ec2 describe-vpcs | jq ".Vpcs[].VpcId" -r # IP's without invited comas
+#aws ec2 describe-vpcs | jq ".Vpcs[].VpcId"    # IP's with invited comas
+#aws ec2 describe-vpcs | jq ".Vpcs[].VpcId" -r # IP's without invited comas
 
 VPCID=$(aws ec2 describe-vpcs | jq ".Vpcs[].VpcId" -r)
 
