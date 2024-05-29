9bb55e812fbe686d870487b4a2676f7304465a75
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-29 10:13:01 +0000

    re-arranged the shell script

--- Shell_scripting/VPC.sh
+++ Shell_scripting/VPC.sh
@@ -2,18 +2,15 @@
 
 #This script is used to get the list of VPCs from AWS
 
-Cat test.sh |jq ".[].ip" -r # finding the ip's from a file
+Cat test.sh | jq ".[].ip" -r # finding the ip's from a file
 
-aws ec2 describe-vpcs |jq ".Vpcs[].VpcId"   # IP's with invited comas
-aws ec2 describe-vpcs |jq ".Vpcs[].VpcId" -r  # IP's without invited comas
+aws ec2 describe-vpcs | jq ".Vpcs[].VpcId"    # IP's with invited comas
+aws ec2 describe-vpcs | jq ".Vpcs[].VpcId" -r # IP's without invited comas
 
-VPCID=$(aws ec2 describe-vpcs |jq ".Vpcs[].VpcId" -r)
+VPCID=$(aws ec2 describe-vpcs | jq ".Vpcs[].VpcId" -r)
 
-
-for VPC in $VPCID
-do
-echo $VPC  # without adding any prefix
-echo "THIS VPC ID is : $VPC" # with pre-fix
-echo "---------"  # print line in b/w ip's
+for VPC in $VPCID; do
+    echo $VPC                    # without adding any prefix
+    echo "THIS VPC ID is : $VPC" # with pre-fix
+    echo "---------"             # print line in b/w ip's
 done
-
