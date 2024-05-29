083480b6f33519a4fcb663c2a5f618955ea2ca5d
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-29 14:39:08 +0530

    Create VPC.sh

---
+++ Shell_scripting/VPC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script is used to get the list of VPCs from AWS
+
+Cat test.sh |jq ".[].ip" -r # finding the ip's from a file
+
+aws ec2 describe-vpcs |jq ".Vpcs[].VpcId"   # IP's with invited comas
+aws ec2 describe-vpcs |jq ".Vpcs[].VpcId" -r  # IP's without invited comas
