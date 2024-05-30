0fbde856186a34f6db9b9cabc48166b970e06288
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-30 11:17:29 +0000

    0 Argument testing

--- Shell_scripting/Arguments.sh
+++ Shell_scripting/Arguments.sh
@@ -13,13 +13,27 @@
 
 #*********************** $@ / $* you can pass multiple Arguments at a time **************************
 
+# for ITEM in $@; do
+#     VPCID=$(aws ec2 describe-vpcs --region $ITEM | jq ".Vpcs[].VpcId" -r)
+#     echo "Lets get the VPC ID $ITEM"
+#     for VPC in $VPCID; do
+#         #echo $VPC                    # without adding any prefix
+#         echo "====================================="
+#         echo "THIS VPC ID is : $VPC"                 # with pre-fix
+#         echo "=====================================" # print line in b/w ip's
+#     done
+# done
+
+#*************** $# count of Arguments you have passed -- if it is 0 ask system to through error  ********
+
 for ITEM in $@; do
     VPCID=$(aws ec2 describe-vpcs --region $ITEM | jq ".Vpcs[].VpcId" -r)
     echo "Lets get the VPC ID $ITEM"
     for VPC in $VPCID; do
         #echo $VPC                    # without adding any prefix
-        echo "====================================="
-        echo "THIS VPC ID is : $VPC"                 # with pre-fix
-        echo "=====================================" # print line in b/w ip's
+        #echo "====================================="
+        echo $VPC
+        #echo "=====================================" # print line in b/w ip's
     done
+    echo "you have passed $# Arguments"
 done
