0c00db8953a6da0f6d0e30fdc36be0aad451d477
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-30 07:30:11 +0000

    shell arguments

---
+++ Shell_scripting/Arguments.sh
@@ -0,0 +1,10 @@
+# Arugments passing $1 -- fixed number of Arguments
+
+VPCID=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r)
+
+for VPC in $VPCID; do
+    #echo $VPC                    # without adding any prefix
+    echo "=====================================" 
+    echo "THIS VPC ID is : $VPC" # with pre-fix
+    echo "====================================="             # print line in b/w ip's
+done
