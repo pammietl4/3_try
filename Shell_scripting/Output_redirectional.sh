8844474e1a2ad8b2f8a44a92de9ecc00f7b0e73e
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-05-31 06:48:54 +0000

    output redirectional

---
+++ Shell_scripting/Output_redirectional.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# standard input   = 0
+# standard output  = 1  -- it will display only successful command data
+# standard error   = 2  -- it will display failed data
+# /dev/null  -- special file ( it will show the output of the code but it will not store the output data )
+# >  -- over writing the file
+# >> -- appending the date to existing file
+
+df -h
+ls -lart
+megastar
+
