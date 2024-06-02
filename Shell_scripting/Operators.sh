3604e377036bf8b22ab2a82ea833708979491c20
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-06-02 06:20:35 +0000

    operators file added

---
+++ Shell_scripting/Operators.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# # ******* Arithmetic Operators ********
+# + (Addition )       -- 4th priority
+# - (substraction)    -- 5th priority (least priority)
+# * (multiplication)  -- 1st priority
+# / (division)        -- 2nd priority
+# % (modules)         -- 3rd priority  == division 2)10(5
+#                                                    10
+#                                                    --
+#                                                     0
+
