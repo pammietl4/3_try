97f8cad756c0f4130b1f1de1e45b7b8bf9bb410e
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-06-21 11:00:56 +0000

    port forwarding comment added in commands.sh

--- K8s/class5-pods/commands.sh
+++ K8s/class5-pods/commands.sh
@@ -2,3 +2,5 @@ Imperative format (command execution)
 #Creation of pod 
 kubectl run testpod1 --image=nginx
 #deletion of pod
+
+kubectl port-forward testpod1 --address 80:80  (is used only for testing purpose not for production scenarios)
