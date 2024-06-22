a04db9dd27046233cefbb5496646796b6841dd68
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-06-22 06:53:49 +0000

    port-forward testing

--- K8s/class5-pods/commands.sh
+++ K8s/class5-pods/commands.sh
@@ -3,4 +3,8 @@ Imperative format (command execution)
 kubectl run testpod1 --image=nginx
 #deletion of pod
 
-kubectl port-forward testpod1 --address 80:80  (is used only for testing purpose not for production scenarios)
+#port-forwarding
+kubectl port-forward testpod1 80:80  (is used only for testing purpose not for production scenarios)
+kubectl port-forward testpod1 --address 0.0.0.0 80:80
+kubectl port-forward testpod1 --address 0.0.0.0 80:80 &  (background run)
+kubectl port-forward testpod1 --address 0.0.0.0 8080:80  (localhost)
