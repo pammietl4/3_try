817034e7e66cc0e11e518deffbd9de46630cefda
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-06-22 09:34:37 +0000

    tested port-forward/logs/exec from command.sf file

--- K8s/class5-pods/commands.sh
+++ K8s/class5-pods/commands.sh
@@ -5,6 +5,15 @@ kubectl run testpod1 --image=nginx
 
 #port-forwarding
 kubectl port-forward testpod1 80:80  (is used only for testing purpose not for production scenarios)
-kubectl port-forward testpod1 --address 0.0.0.0 80:80
+kubectl port-forward testpod1 --address 0.0.0.0 80:80    (localhost)
 kubectl port-forward testpod1 --address 0.0.0.0 80:80 &  (background run)
-kubectl port-forward testpod1 --address 0.0.0.0 8080:80  (localhost)
+kubectl port-forward testpod1 --address 0.0.0.0 8080:80  (anyone can access from internet)
+
+#logs
+kubectl logs testpod1
+kubectl logs testpod1 -f  (latest logs)
+
+#enter into pods
+kubectl exec -it testpod2 -- env
+kubectl exec -it testpod2 -- ls -al
+kubectl exec -it testpod2 -- bash
