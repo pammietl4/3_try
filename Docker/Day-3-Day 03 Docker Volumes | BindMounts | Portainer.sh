94c8409b40ac5309e70e19cce6b9385e40d18adc
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-05 12:04:31 +0530

    command for entering into a container

--- Docker/Day-3-Day 03 Docker Volumes | BindMounts | Portainer.sh
+++ Docker/Day-3-Day 03 Docker Volumes | BindMounts | Portainer.sh
@@ -1,2 +1,8 @@
 #Imagine if some reason Database container failed, we will loose all the data to avoild this Problem Volumes came into picture ( we can take the snapshot of the volume)
 
+#command to enter into Database container
+docker exec -t <Image_name/container_id> mongosh
+
+#command to enter into nginx container
+docker exec -t <Image_name/container_id> bash
+
