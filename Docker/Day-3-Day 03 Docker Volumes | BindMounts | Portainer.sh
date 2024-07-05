1adda736b8a795114388bec6cf44998190e8ca5b
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-05 13:34:14 +0530

    step by step process of volume assignment for a conatiner
    this process will help devlopers to protect there data without lose

--- Docker/Day-3-Day 03 Docker Volumes | BindMounts | Portainer.sh
+++ Docker/Day-3-Day 03 Docker Volumes | BindMounts | Portainer.sh
@@ -1,8 +1,73 @@
 #Imagine if some reason Database container failed, we will loose all the data to avoild this Problem Volumes came into picture ( we can take the snapshot of the volume)
 
+#list volumes
+docker volume ls
+
+#create volume
+docker volume create mongodb-vol
+
+#Create container & attach volume to it
+docker run --rm -d --name mongodb -v mongodb-vol:/data/db -p 27017:27017 mongo:latest
+
 #command to enter into Database container
 docker exec -t <Image_name/container_id> mongosh
 
 #command to enter into nginx container
-docker exec -t <Image_name/container_id> bash
+docker exec -it <Image_name/container_id> bash
+
+#Once entered into the mongodb container, check the tables
+show dbs;
+
+#create a table with sample data
+
+db.helo.insertMany([
+{ "_id" : 1, "name" : "Matt", "status": "active", "level": 12, "score":202},
+        	{ "_id" : 2, "name" : "Frank", "status": "inactive", "level": 2, "score":9},
+        	{ "_id" : 3, "name" : "Karen", "status": "active", "level": 7, "score":87},
+        	{ "_id" : 4, "name" : "Katie", "status": "active", "level": 3, "score":27, "status": "married", "emp": "yes", "kids": 3},
+        	{ "_id" : 5, "name" : "Matt1", "status": "active", "level": 12, "score":202},
+        	{ "_id" : 6, "name" : "Frank2", "status": "inactive", "level": 2, "score":9},
+        	{ "_id" : 7, "name" : "Karen3", "status": "active", "level": 7, "score":87},
+        	{ "_id" : 8, "name" : "Katie4", "status": "active", "level": 3, "score":27, "status": "married", "emp": "yes", "kids": 3}
+        	])
+db.helo.find({name: "Katie"})
+
+#validate the newly created table
+show dbs;
+db.helo.find()
+
+#Exit from the database
+exit
+
+#stop the database container
+docker stop mongodb
+
+#create new container & attach the same volume
+docker run --rm -d --name mongodatabase -v mongodb-vol:/data/db -p 27017:27017 mongo:latest
+
+#enter into the conatiner
+docker exec -it mongodatabase mongosh
+
+#validate the old files are exit/ not
+show dbs
+db.helo.find()
+exit
+
+#validate the same from folder path
+cd /dockerdata/volumes/mongodb-vol/_data
+
+#Create snapshot of this volume
+Go to GUI -->Volume -->create snapshot
+
+
+
+
+
+
+
+
+
+
+
+
 
