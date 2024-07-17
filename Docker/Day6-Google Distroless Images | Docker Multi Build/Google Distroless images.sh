2f91f7846e3bc0176788221d01c8d62ea46f2dac
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-17 07:38:41 +0000

    multibuild file created

--- Docker/Day6-Google Distroless Images | Docker Multi Build/Google Distroless images.sh
+++ Docker/Day6-Google Distroless Images | Docker Multi Build/Google Distroless images.sh
@@ -12,5 +12,22 @@ we can not go & login into the container ( docker exec -it app1 bash)
 https://github.com/GoogleContainerTools/distroless
 
 
+FROM gcr.io/distroless/static-debian11
+LABEL name="pammi"
+RUN mkdir /app
+RUN groupadd appuser1 && useradd -r -g appuser1 appuser1
+WORKDIR /app
+USER appuser1
+ENV AWS_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE \
+    AWS_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY \
+    AWS_DEFAULT_REGION=ap-southeast-1
+ARG T_VERSION='1.6.6' \
+    P_VERSION='1.8.0'
+RUN apt update && apt install -y jq net-tools unzip curl wget \
+    && apt install -y nginx iputils-ping
+
+EXPOSE 80
+
+CMD [ "nginx","-g","daemon off;" ]
 
 
