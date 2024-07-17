11fd48323ca6c385246ec6221ab765df4ca0b4b8
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-17 07:09:56 +0000

    explained about google distroless images

---
+++ Docker/Day6-Google Distroless Images | Docker Multi Build/Google Distroless images.sh
@@ -0,0 +1,16 @@
+
+Google distroless images are extremly light in weight in nature (2mb)
+
+why these images are less in weight (it doesn't contain shell/bash in it)
+
+we can not go & login into the container ( docker exec -it app1 bash)
+
+# these images are using for security practices
+
+# you can not install any packages / softwares on top this images
+
+
+
+
+
+
