454202c890cd8bf31b0483d47987229fcc0ee7c3
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-17 06:59:44 +0000

    process of building docker image

--- Docker/Day6-Google Distroless Images | Docker Multi Build/Building_docker_images_process.sh
+++ Docker/Day6-Google Distroless Images | Docker Multi Build/Building_docker_images_process.sh
@@ -1,1 +1,7 @@
-########################################################
+Step1 -- Developer will devlop the code
+Step2 -- Code will be tested (SAST) via SonarQube/Checkmarx
+Step3 -- once the code is tested, it is free from Vulnurabilities we can (build) this code into a War/Jar file (Artifact) using Maven
+Step4 -- we need to wirte a docker file to copy this Artifact into container
+Step5 -- we need to build the docker image using above docker file
+Step6 -- Once docker image is build. we need to push this image into docker hub/Amazon ECR
+
