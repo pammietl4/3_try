42c304a7bbb652a99f1fa5fcb6d7ae0317683526
pammietl4 <63113628+pammietl4@users.noreply.github.com>
2024-07-22 07:46:34 +0000

    added docker swarm commands

---
+++ Docker/Day8-Docker Swarm part 1 | RAFT Algorithm | Swarm Commands/docker_swarm_validation.sh
@@ -0,0 +1,16 @@
+# list Manager/worker nodes
+docker node ls
+
+# Create nginx conatiner using swarm
+docker service create --name app1 --replicas 6 --publish 8000:80 nginx
+docker service create --name app2 --replicas 2 --publish 8001:80 httpd
+
+# list services
+docker service ls
+
+# list the replicas of the service
+docker service ps app1
+
+# increase/ decrease of the replicas
+docker service scale app1=3
+docker service scale app1=6
