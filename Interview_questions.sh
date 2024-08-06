5cf12626b02e3d862955b5d51dbff82126a11b9a
Pammi Lakshmareddy <63113628+pammietl4@users.noreply.github.com>
2024-08-06 22:53:18 +0530

    Interview questions

---
+++ Interview_questions.sh
@@ -0,0 +1,30 @@
+1. Have you ever managed an application single-handedly?
+2. What are the benefits of Infrastructure as Code (IaC)?
+3. What are the different ways to create infrastructure as code?
+4. What is the difference between public and private networking?
+5. What is a Docker registry and why do we need it?
+6. What is a secrets manager?
+7. What is the secure way to manage sensitive information?
+8. Have you worked with Kubernetes (K8s)?
+9. What is the difference between Docker and Kubernetes?
+10. Can you explain an end-to-end deployment for an application?
+11. If you want to use Kubernetes instead of EC2 instances, how would you do it? Have you used Helm charts or other CD tools? How would you handle a project with multiple microservices on Kubernetes?
+12. How do you connect a bastion host to a private network? Can you explain VPC and VPC peering?
+13. Have you configured a system where code is automatically merged and published upon a developer completing a ticket in Jira? What exactly have you managed?
+14. How do you set up Nginx on a server?
+15. What is a load balancer and its benefits? What is Cloud NAT?
+16. What is the difference between a load balancer and a Cloud NAT gateway?
+17. How do you see yourself fitting into this particular role?
+18. Can you share an instance where you provided a solution for cost optimization while managing resource allocation?
+19. Describe a situation where the entire production instance crashed, and you had to fix it quickly. Have you experienced such a scenario?
+20. What is blue-green deployment and why is it needed?
+21. What other deployment strategies do you know?
+22. What advanced AWS resource types have you worked with and utilized?
+23. How are hosted modules (like AI/ML) deployed, customized, and scaled as per different frontend/backend requirements in AWS with the help of a DevOps engineer?
+24. Can you describe a technology you had not heard of before but managed to learn and use on your own?
+25. What challenges have you faced as a DevOps engineer?
+26. Can you share real-life incidents where you solved an error after working for two or three days?
+27. Have you managed large-scale databases and real-time backups or replication?
+28. During data loss, what strategy do you use to ensure no data loss, especially in critical applications like banking?
+29. Have you faced any cyberattacks on systems you built and implemented? What precautions do you take?
+30. What are the networking setup rules you follow?
